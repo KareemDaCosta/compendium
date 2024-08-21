b339f0b3b575111cdc4f9d694fe5a0c3db4c383b
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2024-08-21 18:05:41 -0400

    Move base data to external file

---
+++ base_data.py
@@ -0,0 +1,250 @@
+data = {
+    "1": {
+        "id": 1,
+        "name": "Goblin",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=goblin",
+        "image": "https://www.aidedd.org/dnd/images/goblin.jpg",
+        "cr": 0.25,
+        "size": "Small",
+        "type": "Humanoid",
+        "alignment": "Neutral Evil",
+        "hp": 7,
+        "ac": 15,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 30
+            }
+        ],
+        "description": "Goblins in Dungeons & Dragons are diminutive humanoid creatures known for their cunning and mischief. Typically standing around 3 to 4 feet tall, goblins have green or gray skin, large pointy ears, and sharp teeth. They are notorious for their love of chaos, often raiding settlements or ambushing travelers for loot. Despite their small stature, goblins compensate with sheer numbers, swarming opponents with overwhelming force when necessary. Goblins are also known for their ability to craft traps and other devious devices, making them a constant threat to those who dare to venture into their territory."
+    },
+    "2": {
+        "id": 2,
+        "name": "Ancient Red Dragon",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=ancient-red-dragon",
+        "image": "https://www.aidedd.org/dnd/images/red-dragon.jpg",
+        "cr": 24,
+        "size": "Gargantuan",
+        "type": "Dragon",
+        "alignment": "Chaotic Evil",
+        "hp": 546,
+        "ac": 22,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 40
+            },
+            {
+                "type": "fly",
+                "distance": 80
+            },
+            {
+                "type": "climb",
+                "distance": 40
+            }
+        ],
+        "description": "Ancient Red Dragons, the apex predators of the draconic world in Dungeons & Dragons, embody sheer might and cunning intellect. With scales as resilient as adamantine and eyes that gleam with ancient wisdom, they are formidable adversaries to any who dare challenge them. Their fiery breath can consume entire landscapes, leaving naught but ash in its wake. Legends speak of their insatiable hunger for power and wealth, driving them to dominate lesser beings and amass vast hoards of treasure within their lairs."
+    },
+    "3": {
+        "id": 3,
+        "name": "Beholder",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=beholder",
+        "image": "https://www.aidedd.org/dnd/images/beholder.jpg",
+        "cr": 13,
+        "size": "Large",
+        "type": "Aberration",
+        "alignment": "Lawful Evil",
+        "hp": 180,
+        "ac": 18,
+        "movement": [
+            {
+                "type": "fly",
+                "distance": 20
+            }
+        ],
+        "description": "Beholders in Dungeons & Dragons are aberrations of pure, unsettling horror. With a large, spherical body covered in numerous eyes and a massive, central eye that emits deadly rays, they inspire fear in all who behold them. These creatures are known for their paranoid and xenophobic nature, often dwelling alone in dark, labyrinthine lairs. Possessing a vast array of magical abilities and a keen intellect, Beholders are formidable adversaries, capable of outwitting even the most skilled adventurers."
+    },
+    "4": {
+        "id": 4,
+        "name": "Tarrasque",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=tarrasque",
+        "image": "https://www.aidedd.org/dnd/images/tarrasque.jpg",
+        "cr": 30,
+        "size": "Gargantuan",
+        "type": "Monstrosity",
+        "alignment": "Unaligned",
+        "hp": 676,
+        "ac": 25,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 40
+            }
+        ],
+        "description": "Tarrasques in Dungeons & Dragons are legendary creatures of unparalleled destruction and dread. Towering over cities with their immense size, they possess nearly impenetrable armor and an insatiable appetite for destruction. With razor-sharp claws and a maw capable of swallowing entire armies whole, they are rightly feared as harbingers of cataclysm. Legends tell of their awakening as heralding the end of civilizations, requiring the combined might of heroes and deities to quell their rampage."
+    },
+    "5": {
+        "id": 5,
+        "name": "Mind Flayer",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=mind-flayer",
+        "image": "https://www.aidedd.org/dnd/images/mind-flayer.jpg",
+        "cr": 7,
+        "size": "Medium",
+        "type": "Aberration",
+        "alignment": "Lawful Evil",
+        "hp": 71,
+        "ac": 15,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 30
+            }
+        ],
+        "description": "Mind Flayers, also known as Illithids, are aberrations of immense psychic power and malevolent intent in Dungeons & Dragons. With their octopus-like heads and tentacled mouths, they are a terrifying sight to behold. Their mastery of psionics allows them to dominate the minds of their victims, turning them into thralls or consuming their brains for sustenance. Illithids are known for their vast, subterranean cities and their insidious plots to enslave or exterminate all other sentient beings."
+    },
+    "6": {
+        "id": 6,
+        "name": "Lich",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=lich",
+        "image": "https://www.aidedd.org/dnd/images/lich.jpg",
+        "cr": 21,
+        "size": "Medium",
+        "type": "Undead",
+        "alignment": "Any Evil",
+        "hp": 135,
+        "ac": 17,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 30
+            }
+        ],
+        "description": "Liches in Dungeons & Dragons are powerful undead spellcasters who have achieved immortality through dark rituals and the phylactery that contains their soul. With their withered, desiccated, and skeletal appearance, they are a haunting sight to behold. Liches are known for their mastery of necromancy and their insatiable thirst for knowledge and power. They often dwell in hidden, labyrinthine lairs, surrounded by legions of undead minions and guarded by powerful magical wards."
+    },
+    "7": {
+        "id": 7,
+        "name": "Purple Worm",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=purple-worm",
+        "image": "https://www.aidedd.org/dnd/images/purple-worm.jpg",
+        "cr": 15,
+        "size": "Gargantuan",
+        "type": "Monstrosity",
+        "alignment": "Unaligned",
+        "hp": 247,
+        "ac": 18,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 50
+            },
+            {
+                "type": "burrow",
+                "distance": 30
+            }
+        ],
+        "description": "Purple Worms in Dungeons & Dragons are colossal subterranean predators, lurking in the depths of the earth. With their massive, serpentine bodies reaching lengths of up to 80 feet or more, they are formidable adversaries for any who venture into their domain. Armed with rows of razor-sharp teeth and a corrosive digestive system, they can devour creatures whole and tunnel through solid rock with ease. Their sudden appearances from beneath the ground strike fear into the hearts of travelers and miners alike, making them one of the most dreaded dangers of the underworld."
+    },
+    "8": {
+        "id": 8,
+        "name": "Mimic",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=mimic",
+        "image": "https://www.aidedd.org/dnd/images/mimic.jpg",
+        "cr": 2,
+        "size": "Medium",
+        "type": "Monstrosity",
+        "alignment": "Unaligned",
+        "hp": 58,
+        "ac": 12,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 15
+            }
+        ],
+        "description": "Mimics in Dungeons & Dragons are shapeshifting monstrosities that disguise themselves as ordinary objects, such as chests or doors. When an unsuspecting victim approaches, the mimic reveals its true form, attacking with adhesive pseudopods and a powerful bite. Their ability to blend seamlessly into their surroundings makes them a constant threat to adventurers and treasure hunters, as they are often found guarding valuable loot or lurking in dark, abandoned places."
+    },
+    "9": {
+        "id": 9,
+        "name": "Rakshasa",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=rakshasa",
+        "image": "https://www.aidedd.org/dnd/images/rakshasa.jpg",
+        "cr": 13,
+        "size": "Medium",
+        "type": "Fiend",
+        "alignment": "Lawful Evil",
+        "hp": 110,
+        "ac": 16,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 40
+            }
+        ],
+        "description": "Rakshasas in Dungeons & Dragons are fiendish shapechangers known for their cunning and malevolence. With their feline heads and humanoid bodies, they are a terrifying sight to behold. Rakshasas are known for their mastery of illusion and their insidious plots to corrupt and manipulate mortals. They often pose as nobles, merchants, or other influential figures, using their powers to sow chaos and discord wherever they go."
+    },
+    "10": {
+        "id": 10,
+        "name": "Gelatinous Cube",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=gelatinous-cube",
+        "image": "https://www.aidedd.org/dnd/images/gelatinous-cube.jpg",
+        "cr": 2,
+        "size": "Large",
+        "type": "Ooze",
+        "alignment": "Unaligned",
+        "hp": 84,
+        "ac": 6,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 15
+            }
+        ],
+        "description": "Gelatinous Cubes in Dungeons & Dragons are amorphous, transparent oozes that lurk in dark, damp places, such as dungeons and sewers. Their ability to dissolve organic matter and inorganic materials alike makes them a constant threat to adventurers and unwary travelers. Their nearly invisible nature and their ability to engulf creatures whole make them a terrifying and deadly hazard to those who dare to explore their lairs."
+    },
+    "11": {
+        "id": 11,
+        "name": "Pit Fiend",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=pit-fiend",
+        "image": "https://www.aidedd.org/dnd/images/pit-fiend.jpg",
+        "cr": 20,
+        "size": "Large",
+        "type": "Fiend",
+        "alignment": "Lawful Evil",
+        "hp": 300,
+        "ac": 19,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 30
+            },
+            {
+                "type": "fly",
+                "distance": 60
+            }
+        ],
+        "description": "Pit Fiends in Dungeons & Dragons are powerful, malevolent fiends known for their cunning and cruelty. With their massive, bat-like wings and fearsome countenance, they are a terrifying sight to behold. Pit Fiends are known for their mastery of infernal magic and their insatiable desire to corrupt and enslave mortal souls. They often serve as generals and commanders in the armies of the Nine Hells, leading legions of lesser devils in their endless war against the forces of good."
+    },
+    "12": {
+        "id": 12,
+        "name": "Balor",
+        "url": "https://www.aidedd.org/dnd/monstres.php?vo=balor",
+        "image": "https://www.aidedd.org/dnd/images/demon-balor.jpg",
+        "cr": 19,
+        "size": "Huge",
+        "type": "Fiend",
+        "alignment": "Chaotic Evil",
+        "hp": 262,
+        "ac": 19,
+        "movement": [
+            {
+                "type": "walk",
+                "distance": 40
+            },
+            {
+                "type": "fly",
+                "distance": 80
+            }
+        ],
+        "description": "Balors in Dungeons & Dragons are towering demons of immense power and malevolence. With flaming whips and massive swords wreathed in hellfire, they are fearsome adversaries to even the most seasoned adventurers. Possessing wings that allow them to soar through the skies with deadly grace, Balors command legions of lesser demons with iron-clad authority. Their very presence instills terror, as they embody the chaotic and destructive forces of the Nine Hells, leaving ruin and despair in their wake."
+    }
+}
